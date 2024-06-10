f9da97bb7d88afde760bc09ee545d665e6ee0c74
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-11 00:53:21 +0200

    Rinomino componenti per un migliore importazione in App.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+    <footer>
+        <div>
+            <h1>Buongiorno mondo</h1>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
