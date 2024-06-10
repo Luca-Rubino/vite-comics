83f5186e61a03a275074b6feea63b03eeccfcabf
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-11 01:40:25 +0200

    Creare componente AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,21 @@
 <script>
-
+export default {
+    data() {
+        return {
+        }
+    },
+}
 </script>
 
 <template>
+
+<main>
+    <div class="content">
+
+        <p>-->content goes here<--</p>
+        
+    </div>
+</main>
 
 </template>
 
