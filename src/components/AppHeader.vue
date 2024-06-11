76c821f6e5debe18b1f50ef6864f38e3c0489288
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-11 11:18:05 +0200

    Inserire lo style del componente AppHeader usando css

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -81,20 +81,60 @@ export default {
     
 <header>
     <div class="container">
+
         <section class="image">
             <img src="../assets/img/dc-logo.png" alt="dc-logo">
         </section>
+
         <section class="list-title">
+
             <ul>
                 <li v-for="(title,index) in headerLink" :key="title.id">
                     {{ title.title }}
                 </li>
             </ul>
+
         </section>
+        
     </div>
 </header>
+
+<main>
+    <div class="content">
+
+        <p>-->Content goes here<--</p>
+        
+    </div>
+</main>
 
 </template>
 
 <style lang="scss" scoped>
+
+header > div {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
+
+section.list-title {
+  display: flex;
+  align-items: center;
+}
+
+ul{
+  display: flex;
+}
+
+li{
+  margin-right: 1rem;
+}
+
+div.content{
+  background-color: black;
+  color: white;
+  padding: 2rem 13rem;
+  font-size: 1.5rem;
+}
+
 </style>
